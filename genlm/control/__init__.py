6b83a4d76ed230de96a9789bf47f1f76a63cace0
benlebrun <benlebrun1@gmail.com>
2025-04-10 15:44:40 -0400

    add top-level imports

--- genlm/control/__init__.py
+++ genlm/control/__init__.py
@@ -1,19 +1,25 @@
 from .constant import EOS, EOT
-from .engine import InferenceEngine
-from .potential import Potential, PromptedLLM, BoolCFG, BoolFSA, WFSA, WCFG
-from .sampler import direct_token_sampler, eager_token_sampler, topk_token_sampler, AWRS
+from .potential import Potential, PromptedLLM, BoolCFG, BoolFSA, WFSA, WCFG, JsonSchema
+from .sampler import (
+    SMC,
+    direct_token_sampler,
+    eager_token_sampler,
+    topk_token_sampler,
+    AWRS,
+)
 from .viz import InferenceVisualizer
 
 __all__ = [
     "EOS",
     "EOT",
-    "InferenceEngine",
+    "SMC",
     "Potential",
     "PromptedLLM",
     "WCFG",
     "BoolCFG",
     "WFSA",
     "BoolFSA",
+    "JsonSchema",
     "AWRS",
     "direct_token_sampler",
     "eager_token_sampler",
